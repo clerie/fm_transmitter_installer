b245ed6c3b7feb013cebb123ceabd2ae391feeb7
clerie <cleriese@gmail.com>
2016-03-03 19:19:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,20 @@
 
 case "$1" in 
     install)
+        echo "Installing fm_transmitter..."
+        echo "Install git..."
+        sudo apt-get install git
+        echo "Go to /home/pi/ ..."
+        sudo cd /home/pi/
+        echo "Downloading data from Github..."
+        sudo git clone https://github.com/markondej/fm_transmitter/
+        echo "Go to /home/pi/fm_transmitter/"
+        sudo cd /home/pi/fm_transmitter/
+        echo "Install make..."
+        sudo apt-get install make gcc g++
+        echo "Compile source..."
+        sudo make
+        echo "Ready!"
         
         ;;
     *)
