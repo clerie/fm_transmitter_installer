e5ab1a99ec648ada1cee6287faf8fefce9e485e7
clerie <cleriese@gmail.com>
2016-03-03 20:12:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
 # Loading status of installation
+# CORE
 if [ -f "/home/pi/fm_transmitter/fm_transmitter" ]
 then
     statusCORE="ok"
 else
     statusCORE="no"
 fi
+# LAUNCHER
+if [ -f "/home/pi/fm_transmitter.sh" ]
+then
+    statusLAUNCHER="ok"
+else
+    statusLAUNCHER="no"
+fi
+# AUTORUN
+if [ -f "/etc/init.d/fm_transmitter.sh" ]
+then
+    statusAUTORUN="ok"
+else
+    statusAUTORUN="no"
+fi
+
+
+
+
+
 
 case "$1" in 
     install)
         case "$2" in
             core)
-                
                 case "$statusCORE" in
                     ok)
                         echo "The core is installed"
@@ -35,6 +54,42 @@ case "$1" in
                     ;;
                 esac
             ;;
+            launcher)
+                case "$statusLAUNCHER" in
+                    ok)
+                        echo "The launcher is installed"
+                    ;;
+                    *)
+                        echo "The launcher is not installed"
+                        case "$statusCORE" in
+                            ok)
+                                echo "The core is installed"
+                            ;;
+                            *)
+                                echo "The core is not installed"
+                            ;;
+                        esac
+                    ;;
+                esac
+            ;;
+            autorun)
+                case "$statusAUTORUN" in
+                    ok)
+                        echo "The autorun is installed"
+                    ;;
+                    *)
+                        echo "The autorun is not installed"
+                        case "$statusLAUNCHER" in
+                            ok)
+                                echo "The launcher is installed"
+                            ;;
+                            *)
+                                echo "The launcher is not installed"
+                            ;;
+                        esac
+                    ;;
+                esac
+            ;;
         esac
     ;;
     *)
@@ -47,10 +102,10 @@ case "$1" in
         echo "                          Installation status: $statusCORE"
         echo "install launcher      install an start-file in /home/pi/"
         echo "                          - provides an easy to use launcher for wav and mp3"
-        echo "                          Installation status: no"
+        echo "                          Installation status: $statusLAUNCHER"
         echo "install autorun       install an autorun-file in /etc/init.d/"
         echo "                          - running on default values of launcher"
-        echo "                          Installation status: no"
+        echo "                          Installation status: $statusAUTORUN"
         exit 1
     ;;
 esac
